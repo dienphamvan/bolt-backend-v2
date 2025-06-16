d2011b56798c1f0cf833c2b315f8e02fbdfb0d26
dienpv <chuanthanh133@gmail.com>
2025-06-16 23:48:26 +0700

    chore: add script and health-check-api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CustomerModule } from './customer/customer.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CarModule } from './car/car.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [CustomerModule, PrismaModule, CarModule],
+  controllers: [AppController],
 })
 export class AppModule {}
